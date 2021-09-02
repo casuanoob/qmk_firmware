078a341da365e9ec030c6dac1919ee1db3f4e818
Charly Delay <0xcharly@users.noreply.github.com>
2021-09-02 16:01:56 +0900

    users/delay: initial implementation for COMPOSE

--- users/delay/compose.c
+++ users/delay/compose.c
@@ -1,24 +1,8 @@
-/**
- * Copyright 2021 Charly Delay <charly@codesink.dev> (@0xcharly)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 #include "compose.h"
 #include "delay.h"
 
 /**
- * \brief Resets the compose state, clears the queue.
+ * Resets the compose state, clears the queue.
  */
 void _compose_reset(compose_state_t *state) {
   for (int i = 0; i < COMPOSE_KEYCODE_QUEUE_SIZE; ++i) {
@@ -30,7 +14,7 @@ void _compose_reset(compose_state_t *state) {
 }
 
 /**
- * \brief Process the queue.
+ * Process the queue.
  *
  * Resets the compose state regardless of whether a compose sequence was
  * matched.
@@ -41,7 +25,7 @@ void _compose_process_queue(compose_state_t *state) {
 }
 
 /**
- * \brief Push a new keycode to the queue.
+ * Push a new keycode to the queue.
  */
 void _compose_push_keycode(compose_state_t *state, uint16_t keycode) {
   state->keycode_queue[state->keycode_queue_index++] = keycode;
@@ -49,14 +33,14 @@ void _compose_push_keycode(compose_state_t *state, uint16_t keycode) {
 }
 
 /**
- * \brief Whether the compose queue is full.
+ * Whether the compose queue is full.
  */
 bool _compose_queue_is_full(compose_state_t *state) {
   return state->keycode_queue_index >= COMPOSE_KEYCODE_QUEUE_SIZE;
 }
 
 /**
- * \brief Handle a keypress when composing is active.
+ * Handle a keypress when composing is active.
  *
  * If the keycode is eligible, push it into the compose queue.  When the queue
  * is full, trigger composing (see `_compose_process_queue(…)`).
@@ -64,36 +48,32 @@ bool _compose_queue_is_full(compose_state_t *state) {
 bool _compose_handle_keypress(compose_state_t *state, uint16_t keycode,
                               keyrecord_t *record) {
   // Get the base keycode of a mod or layer tap key.
-  switch (keycode) {
-    case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
-    case QK_MOD_TAP ... QK_MOD_TAP_MAX:
-      // Early return if this has not been considered tapped yet.
-      if (record->tap.count == 0) {
-        return true;
-      }
-      keycode = keycode & 0xff;
+  if ((QK_MOD_TAP <= keycode && keycode <= QK_MOD_TAP_MAX) ||
+      (QK_LAYER_TAP <= keycode && keycode <= QK_LAYER_TAP_MAX)) {
+    // Earlier return if this has not been considered tapped yet.
+    if (record->tap.count == 0) {
+      return true;
+    }
+    keycode = keycode & 0xff;
+  }
+  // Early exit if the keycode is not eligible (ie. not a basic keycode or
+  // a mod).
+  if ((keycode < KC_A || keycode > KC_CAPSLOCK) &&
+      (keycode < QK_MODS || keycode > QK_MODS_MAX)) {
+    return true;
   }
   // Force compose on Enter.
   if (keycode == KC_ENT) {
     _compose_process_queue(state);
     return false;
   }
-  // Process eligible keycodes (ie. basic keycodes and mods).
-  switch (keycode) {
-    case KC_A ... KC_CAPSLOCK:
-    case QK_MODS ... QK_MODS_MAX:
-      // Shift keycode if necessary, ignores other mods.
-      if (mod_config(get_mods() | get_oneshot_mods()) & MOD_MASK_SHIFT) {
-        keycode = S(keycode);
-      }
-      // Push keycode into the queue, and compose if the queue is full.
-      _compose_push_keycode(state, keycode);
-      if (_compose_queue_is_full(state)) {
-        _compose_process_queue(state);
-      }
-      return false;
+  // Push the keycode to the queue, and compose if the queue is full.
+  _compose_push_keycode(state,
+                        get_mods() & MOD_MASK_SHIFT ? S(keycode) : keycode);
+  if (_compose_queue_is_full(state)) {
+    _compose_process_queue(state);
   }
-  return true;
+  return false;
 }
 
 void keyboard_post_init_compose(compose_state_t *state) {
@@ -136,6 +116,6 @@ bool compose_queue_equal(compose_state_t *state,
 }
 
 /**
- * \brief This is where compose combos should defined.
+ * This is where compose combos should defined.
  */
 __attribute__((weak)) void compose_user(compose_state_t *state) {}
