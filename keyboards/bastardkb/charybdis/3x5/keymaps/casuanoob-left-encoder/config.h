77f509f775533c8b26c1efad7b70cd2cbb874fb2
casuanoob <gms.rulz@hotmail.com>
2022-05-19 02:21:53 +1000

    add left encoder keymap

---
+++ keyboards/bastardkb/charybdis/3x5/keymaps/casuanoob-left-encoder/config.h
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2022 Charly Delay <charly@codesink.dev> (@0xcharly)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+// Trackball angle adjustment.
+#undef ROTATIONAL_TRANSFORM_ANGLE
+#define ROTATIONAL_TRANSFORM_ANGLE -25
+#define POINTING_DEVICE_INVERT_X
+
+// Custom DPI.
+#define CHARYBDIS_MINIMUM_DEFAULT_DPI 600
+#define CHARYBDIS_MINIMUM_SNIPING_DPI 400
+
+/* Override RGB settings. 
+ * \brief 12 LEDs are added by the falcon PCB to the original 36
+*/
+#undef RGBLED_NUM 
+#undef RGBLED_SPLIT
+#define RGBLED_NUM 48
+#define RGBLED_SPLIT \
+    { 30, 18 }
+
+/* Define encoder pads. 
+ * \brief Encoder pads wired to MISO and SCLK
+
+#define ENCODERS_PAD_A { A7 }
+#define ENCODERS_PAD_B { A6 }
+*/
+// Disable unused keycodes.
+//#define NO_CHARYBDIS_KEYCODES
